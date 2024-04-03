7333deb3ac2d1fc7d584f662ffbbd52c827c0722
Nathan Soucier <soucienp@gmail.com>
2024-04-03 11:13:52 -0400

    updated code to use flask session

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect, flash
+from flask import session, make_response
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey, personality_quiz, surveys
 
@@ -18,11 +19,15 @@ def show_form():
 @app.route('/questions/<int:qID>')
 def display_question(qID):
     """Displays question for user"""
-    if (qID != len(responses)): # true if user edited url for invalid question
+    qnumber = len(session['responses'])
+    
+    if (qID != len(session['responses'])): # true if user edited url for invalid question
         flash("Invalid. Cannot access the requested question.")
-        return redirect(f'/questions/{len(responses)}')
-    elif (len(responses) >= num_questions): # true if the user is done and changes url to show a question
+        return redirect(f'/questions/{qnumber}')
+    
+    elif (len(session['responses']) >= num_questions): # true if the user is done and changes url to show a question
         return redirect('/done')
+    
     else: # display next question for user
         return render_template('question.html', qID=qID, survey=satisfaction_survey, qty=num_questions)
 
@@ -31,12 +36,25 @@ def add_answer():
     """post method that saves survey answer and moves to next question"""
     ans = list(request.form.keys())[0]
     responses.append(ans)
-    if (len(responses) >= num_questions): # Shows final page if no more questions
-        return redirect('/done')
-    else: # moves to next question
-        return redirect(f'/questions/{len(responses)}')
+    session['responses'] = responses
+    qnumber = len(session['responses'])
     
+    if (len(session['responses']) >= num_questions): # Shows final page if no more questions
+        return redirect('/done')
+    
+    else: # moves to next question
+        return redirect(f'/questions/{qnumber}')
+
+@app.route('/storage', methods=['POST'])
+def store_data():
+    """stores survery answers on session"""
+    session['responses']=[]
+    
+    return redirect('/questions/0')
+    
+
 @app.route('/done')
 def display_done():
     """Page to show user that the survery is done"""
+    
     return '<h1>Survey completed, thank you!</h1>'
